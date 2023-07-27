7d5b9339fc94cabe5a429be45a0e982b84f99d3f
Sai Ganesh <105867763+saigani219@users.noreply.github.com>
2023-07-27 19:45:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ let dropdown = document.getElementById("colorSelect");
+
+let selection = document.querySelector("form > input");
+selection.addEventListener("click", () => {
+	let obj = document.getElementById("colorSelect");
+	let selectedIndex = obj.selectedIndex;
+	
+      // Remove the selected option from the dropdown list
+      obj.remove(selectedIndex);
+});
